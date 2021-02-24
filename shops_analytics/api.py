0b41934d85c64b9413bdfa5f86a506b6dbc2b9c7
andrii <kostenko.andrii.work@gmail.com>
2021-02-24 14:33:25 +0200

    edit logic

--- shops_analytics/api.py
+++ shops_analytics/api.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import AllowAny
 from rest_framework import status, views
 from rest_framework.response import Response
-from django.db.models import Q
+from django.db.models import Q, Sum, Count, Avg, F, ExpressionWrapper, fields
 from .models import DataProductsale
 import pandas as pd
 import json
@@ -24,42 +24,40 @@ class ShopAnalytics(views.APIView):
 
         if not queryset.exists():
             return Response({'message': 'records not found'}, status=status.HTTP_200_OK)
+        df = queryset.values('shop').annotate(
+            avg_price=Avg('margin_price_total'),
+            count=Count('id')
+        )
+        df1 = queryset.values('shop', 'product') \
+            .annotate(total=Sum('margin_price_total'))
 
-        df = df1 = pd.DataFrame(list(queryset.values('total_price', 'qty', 'shop', 'product')))
-
-        df['profit'] = df['total_price'] * df['qty']
-        df['profit'] = df['profit'].apply(pd.to_numeric)
-        df = df.groupby('shop').agg(avg_profit=('profit', 'mean'), total_sales=('shop', 'count'))
+        df = pd.DataFrame(list(df))
+        df1 = pd.DataFrame(list(df1))
 
         def abc_stat(agg_df):
-            product_id_index = 0
-            products_categories = {
-                'A': {'max_val': 80, 'min_val': 0, 'name': 'A'},
-                'B': {'max_val': 95, 'min_val': 80, 'name': 'B'},
-                'C': {'max_val': 100,  'min_val': 95, 'name': 'C'},
-            }
+            agg_df['percent'] = agg_df.total * 100 / agg_df.total.sum()
+            agg_df = agg_df.sort_values(by=['percent'], ascending=False, ignore_index=True)
 
-            agg_df['percent'] = agg_df.product_profit_sum * 100 / agg_df.product_profit_sum.sum()
-            agg_df = agg_df.sort_values(by=['percent'], ascending=False)
+            for i, _ in agg_df.iterrows():
+                try:
+                    agg_df.loc[i, 'percent'] += agg_df.loc[i-1, 'percent']
+                except KeyError:
+                    continue
 
-            category_count = 0
-            abc = {}
+            agg_df.loc[agg_df['percent'] <= 80, 'Category'] = "A"
+            agg_df.loc[(80 < agg_df['percent']) & (95 >= agg_df['percent']), 'Category'] = "B"
+            agg_df.loc[(95 < agg_df['percent']), 'Category'] = "C"
 
-            for i, row_value in agg_df['percent'].iteritems():
-                category_count += row_value
+            m = agg_df[['product', 'Category']].to_json(orient="index")
+            parsed = json.loads(m)
+            return json.dumps(parsed)
 
-                if products_categories['A']['min_val'] <= category_count <= products_categories['A']['max_val']:
-                    abc[i[product_id_index]] = products_categories['A']['name']
-                elif products_categories['B']['min_val'] < category_count <= products_categories['B']['max_val']:
-                    abc[i[product_id_index]] = products_categories['B']['name']
-                elif products_categories['C']['min_val'] < category_count <= products_categories['C']['max_val']:
-                    abc[i[product_id_index]] = products_categories['C']['name']
-
-            return json.dumps(abc)
-
-        df1 = df1.groupby(['product', 'shop']).agg(product_profit_sum=('profit', 'sum'))
         df1 = df1.groupby('shop').agg(abc_stat)
-        df1 = df1.rename(columns={'product_profit_sum': 'abc'})
+        df1['shop'] = df1.index
+        df1 = df1.drop(columns=['product'])
+        df1 = df1.rename(columns={'total': 'abc'})
+        df1.reset_index(drop=True, inplace=True)
 
-        res_data = df.join(df1).to_json(orient="index")
+        res_data = df.merge(df1, how='left', on='shop').drop_duplicates(['shop'], keep='first').to_json(orient="index")
+
         return Response(json.loads(res_data), status=status.HTTP_200_OK)
